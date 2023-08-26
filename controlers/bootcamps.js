41f46b70489e30d53e00a8210ead26c6f3794b5b
RJ Robinson <rj@localhost.localdomain>
2023-08-26 16:38:14 -0700

    Get All Bootcamps from Mongo

--- controlers/bootcamps.js
+++ controlers/bootcamps.js
@@ -3,12 +3,26 @@ const Bootcamp = require("../models/Bootcamp");
 
 // Controlers
 
-exports.getBootcamps = (req,res) => {
-    res.send("All Bootcamps")
+// @ Get all Bootcamps
+//
+exports.getBootcamps = async (req,res) => {
+    
+    try {
+        const bootcamp = await Bootcamp.find();
+        
+        res.status(200).json({success:true, data:bootcamp})
+    } catch  {
+
+        res.status(400).json({success:false})
+        
+    }
 }
 
+// @ Create a Bootcamp
+//
 exports.addBootcamps = async (req,res,next) => {
-   
+  
+    //Try to create
 
     try {
         const bootcamp = await Bootcamp.create(req.body)
@@ -22,17 +36,22 @@ exports.addBootcamps = async (req,res,next) => {
         res.status(400).json({success:false})
     }
 
-    
 }
 
+// @ Edit a Bootcamp
+//
 exports.editBootcamps = (req,res) => {
     res.send(`Bootcamp id = ${req.params.id}`)
 }
 
+//@ Delete a bootcamp
+//
 exports.deleteBootcamps = (req,res) => {
     res.send(`Bootcamp id to delete = ${req.params.id}`)
 }
 
+// @ Get a Bootcamp by ID
+//
 exports.getBootcamp = (req,res) => {
     res.send(`Get Bootcamp id  = ${req.params.id}`)
 }
