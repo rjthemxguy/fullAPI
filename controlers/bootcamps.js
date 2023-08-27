6d64fc2752ca4b01b14374af0114c14eafcdf492
RJ Robinson <rj@localhost.localdomain>
2023-08-27 15:44:17 -0700

    GEOCode

--- controlers/bootcamps.js
+++ controlers/bootcamps.js
@@ -1,51 +1,41 @@
 const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/errorResponse")
-
+const asyncHandler = require("../middleware/async")
 
 // Controllers
 
 // @ Get all Bootcamps
 //
-exports.getBootcamps = async (req,res,next) => {
-    
-    try {
+exports.getBootcamps = asyncHandler(async (req,res,next) => {
+      
         const bootcamp = await Bootcamp.find();
         
         res.status(200).json({success:true, count:bootcamp.length,data:bootcamp})
-    } catch (err) {
-
-       next(err)
-        
-    }
-}
+          
+});
 
 // @ Create a Bootcamp
 //
-exports.addBootcamps = async (req,res,next) => {
+exports.addBootcamps = asyncHandler(async (req,res,next) => {
   
-    //Try to create
-
-    try {
+    
         const bootcamp = await Bootcamp.create(req.body)
 
         res.status(201).json({
             success:true,
             data:bootcamp
         })
-        
-    } catch (err) {
-        next(err)
-    }
+   
 
-}
+});
 
 // @ Edit a Bootcamp
 //
-exports.editBootcamps = async (req,res,next) => {
+exports.editBootcamps = asyncHandler(async (req,res,next) => {
 
     
 
-    try {
+  
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
             new:true,
             runValidators:true
@@ -57,22 +47,16 @@ exports.editBootcamps = async (req,res,next) => {
             return next(new ErrorResponse(`Bootcamp not found with ID of ${req.params.id}`,404))
        }
 
-
-        
-    } catch (err) {
-        next(err)
-    }
-
-  
+     
 
    
-}
+});
 
 //@ Delete a bootcamp
 //
-exports.deleteBootcamps = async(req,res,next) => {
+exports.deleteBootcamps = asyncHandler(async(req,res,next) => {
 
-    try {
+    
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id) 
     
        res.status(200).json({success:true, data:{}})
@@ -83,33 +67,23 @@ exports.deleteBootcamps = async(req,res,next) => {
 
 
         
-    } catch (err) {
-
-        next(err)
-    }
    
-}
+});
 
 // @ Get a single Bootcamp by ID
 //
-exports.getBootcamp = async (req,res,next) => {
+exports.getBootcamp = asyncHandler(async (req,res,next) => {
 
       
-    try {
+   
         const bootcamp = await Bootcamp.findById(req.params.id)
         res.status(200).json({success:true, data:bootcamp})
 
         if(!bootcamp) {
            return next(new ErrorResponse(`Bootcamp not found with ID of ${req.params.id}`,404))
         }
-
-    } catch (err) {
-     
-       next(err)
-    }
+ 
 
    
-
-   // 
-}
+});
 
