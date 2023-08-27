64bfed47c943c24b4a0a332c01e5990ba6322d63
RJ Robinson <rj@localhost.localdomain>
2023-08-27 09:35:35 -0700

    More custon error code

--- middleware/error.js
+++ middleware/error.js
@@ -12,6 +12,18 @@ error.message = err.message;
         error = new ErrorResponse(message, 404);
     }
 
+    //Mongoose duplicate ID
+    if(err.code===11000) {
+        const message = "Duplicate ID in Mongo";
+        error = new ErrorResponse(message, 400);
+    }
+
+    //Mongoose validation error
+    if(err.code==="ValidationError") {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400);
+    }
+
     res.status(error.statusCode || 500).json({
         success:false,
         error:error.message || "Server Error"
