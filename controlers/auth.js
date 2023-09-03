de79603e6b72fa721c9f0f171953fe71c1afe7a9
rjthemxguy <rjrseo@gmail.com>
2023-09-03 01:32:45 +0000

    Create User record pt1

--- controlers/auth.js
+++ controlers/auth.js
@@ -7,6 +7,18 @@ const User = require('../models/User');
 // @ access PUBLIC
 
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({success:"true"});
+    const {name, email, password, role} = req.body;
+
+
+   const user = await User.create({
+        name,
+        email,
+        password,
+        role
+   })
+    
+
+    res.status(200).json({success:true})
   });
   
+
